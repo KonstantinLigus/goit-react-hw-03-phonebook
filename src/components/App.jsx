fed2ff9386ba51f757ce68b1650f975a3e875218
Konstantin Ligus <kostya1989com@gmail.com>
2022-09-05 15:36:56 +0300

    return old commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,83 +1,92 @@
 import { nanoid } from 'nanoid';
-import { useEffect, useState } from 'react';
+import { Component } from 'react';
 import { Box } from './Box';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
-const initValue = () => {
-  const contactsLocalStorage = localStorage.getItem('contacts');
-  if (
-    contactsLocalStorage === '' ||
-    JSON.parse(contactsLocalStorage).length === 0
-  ) {
-    return [
+export class App extends Component {
+  state = {
+    contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ];
+    ],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) {
+      this.setState({ contacts });
+    }
   }
-  return JSON.parse(contactsLocalStorage);
-};
 
-export function App() {
-  const [contacts, setContacts] = useState(initValue);
-  const [filter, setFilter] = useState('');
-  const filteredContacts = filterContacts();
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  function changeFilterHandler(e) {
+  changeFilterHandler = e => {
     const {
-      target: { value },
+      target: { value, name },
     } = e;
-    setFilter(value);
-  }
+    this.setState({ [name]: value });
+  };
 
-  function submitHandler(name, number) {
-    if (contacts.some(contact => contact.name.includes(name))) {
+  submitHandler = (name, number) => {
+    if (this.state.contacts.some(contact => contact.name.includes(name))) {
       alert(`${name} is already in contacts`);
       return;
     }
 
-    setContacts(prevContacts => [
-      { id: nanoid(), name, number },
-      ...prevContacts,
-    ]);
-  }
+    this.setState(prevState => ({
+      contacts: [{ name, number, id: nanoid() }, ...prevState.contacts],
+    }));
+  };
 
-  function filterContacts() {
-    const filterValue = filter.toLowerCase();
-    return contacts.filter(contact =>
+  filterContacts = () => {
+    const filterValue = this.state.filter.toLocaleLowerCase();
+    return this.state.contacts.filter(contact =>
       contact.name.toLocaleLowerCase().includes(filterValue)
     );
-  }
+  };
 
-  function deleteBtnHandler(e) {
-    setContacts(contacts.filter(contact => contact.id !== e.target.id));
-  }
+  deleteBtnHandler = e => {
+    this.setState({
+      contacts: this.state.contacts.filter(
+        contact => contact.id !== e.target.id
+      ),
+    });
+  };
 
-  return (
-    <>
-      <Box p="20px">
-        <Box mb="20px" as="h1">
-          Phonebook
+  render() {
+    const { filter } = this.state;
+    const filteredContacts = this.filterContacts();
+    return (
+      <>
+        <Box p="20px">
+          <Box mb="20px" as="h1">
+            Phonebook
+          </Box>
+          <ContactForm submitHandler={this.submitHandler} />
         </Box>
-        <ContactForm submitHandler={submitHandler} />
-      </Box>
-      <Box p="20px">
-        <Box mb="20px" as="h2">
-          Contacts
+        <Box p="20px">
+          <Box mb="20px" as="h2">
+            Contacts
+          </Box>
+          <Filter
+            filter={filter}
+            changeFilterHandler={this.changeFilterHandler}
+          />
+          <ContactList
+            contacts={filteredContacts}
+            deleteBtnHandler={this.deleteBtnHandler}
+          />
         </Box>
-        <Filter filter={filter} changeFilterHandler={changeFilterHandler} />
-        <ContactList
-          contacts={filteredContacts}
-          deleteBtnHandler={deleteBtnHandler}
-        />
-      </Box>
-    </>
-  );
+      </>
+    );
+  }
 }
